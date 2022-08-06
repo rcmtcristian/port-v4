ce8e16b4704fae03aa54be464b1d004592d47e9a
rcmtcristian <rcmtcristian@gmail.com>
2022-08-06 17:35:17 -0400

    fonts dont work

--- js/main.js
+++ js/main.js
@@ -33,16 +33,10 @@ const scroll = new SmoothScroll('.scrolly [href*="#"]', {
 })();
 
 
-// //option to smoth scroll 
-// $('#nav a').on('click', function(e) {
-//   if(this.hash !== '') {
-//    e.preventDefault();
- 
-//    const hash = this.hash;
- 
-//    $('html, body').animate({
-//      scrollTop: $(hash).offset().top
-//    }, 800
-//    );
-//   }
-//  });
+
+let twit = document.getElementById('twit')
+
+
+twit.addEventListener('mouseover', function(){
+  twit.classList.toggle('animate__rubberBand')
+})
